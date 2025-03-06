77c0d506279c81e0ecbf4c5e179dcec74c9cfe4b
x287x <115352423+x287x@users.noreply.github.com>
2025-03-06 05:14:13 +0300

    Add some basic input components

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import Form from "./components/Form";
+
+function App() {
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    history.replaceState({ submitted: false }, "", document.location.href);
+
+    function onPopstate(e: PopStateEvent) {
+      if (e.state !== undefined) {
+        setSubmitted(e.state.submitted);
+      }
+    }
+
+    window.addEventListener("popstate", onPopstate);
+
+    return () => {
+      window.removeEventListener("popstate", onPopstate);
+    };
+  }, []);
+
+  return (
+    <main>
+      {/* <button
+        className="text-5xl text-white"
+        onClick={() => {
+          setSubmitted(true);
+          history.pushState({ submitted: true }, "", "submitted");
+        }}
+      >
+        Test
+      </button> */}
+
+      {/* Logo */}
+      <img
+        src="/assets/images/logo-full.svg"
+        className="mx-auto w-40 py-8 md:w-auto"
+      />
+
+      {!submitted ? (
+        <Form />
+      ) : (
+        <h2 className="text-6xl text-white">Submitted</h2>
+      )}
+
+      {/* <div>
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div> */}
+    </main>
+  );
+}
+
+export default App;
