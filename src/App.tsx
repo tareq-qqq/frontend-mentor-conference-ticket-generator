53d7dc946c7db1bcff1e133e0b377f1ab5729480
x287x <115352423+x287x@users.noreply.github.com>
2025-03-07 22:13:02 +0300

    Add fronted mentor footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router";
 import { useLocation } from "react-router";
 import Logo from "../assets/images/logo-full.svg";
+import FrontendMentorFooter from "./components/ui/FrontendMentorFooter";
 
 function App() {
   const location = useLocation();
@@ -12,6 +13,8 @@ function App() {
       <img src={Logo} className="mx-auto w-40 py-8 md:w-40 lg:pb-12" />
 
       <Outlet />
+
+      <FrontendMentorFooter />
     </main>
   );
 }
